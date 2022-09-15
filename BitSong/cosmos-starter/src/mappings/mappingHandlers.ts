3b34f7df04140ff081d6ab065cf238029f9006bc
angelorc <angelo@rcstart.it>
2022-09-15 19:55:59 +0200

    (fantoken): add msgIssue

--- BitSong/cosmos-starter/src/mappings/mappingHandlers.ts
+++ BitSong/cosmos-starter/src/mappings/mappingHandlers.ts
@@ -1,12 +1,42 @@
-import { Transaction, Fantoken, FantokenIssueEvent } from "../types";
+import { Fantoken } from "../types";
 import {
   CosmosEvent,
-  CosmosBlock,
   CosmosMessage,
-  CosmosTransaction,
 } from "@subql/types-cosmos";
 
-export async function handleBlock(block: CosmosBlock): Promise<void> {
+export async function handleMessage(msg: CosmosMessage): Promise<void> {
+  const messageRecord = Fantoken.create({
+    id: `${msg.tx.hash}-${msg.idx}`,
+    blockHeight: BigInt(msg.block.block.header.height),
+    txHash: msg.tx.hash,
+    name: msg.msg.decodedMsg.name,
+    symbol: msg.msg.decodedMsg.symbol,
+    denom: "",
+    max_supply: msg.msg.decodedMsg.maxSupply,
+    uri: msg.msg.decodedMsg.uri,
+    authority: msg.msg.decodedMsg.authority,
+    minter: msg.msg.decodedMsg.minter,
+    createdAt: msg.block.block.header.time
+  });
+  await messageRecord.save();
+}
+
+export async function handleEvent(event: CosmosEvent): Promise<void> {
+  const messageRecord = await Fantoken.get(`${event.tx.hash}-${event.msg.idx}`)
+  for(const attr of event.event.attributes) {
+    switch(attr.key) {
+      case "denom":
+        messageRecord.denom = attr.value.slice(1, -1);
+        break;
+      default:
+        break;
+    }
+  }
+  await messageRecord.save();
+}
+
+
+/*export async function handleBlock(block: CosmosBlock): Promise<void> {
   // If you wanted to index each block in Cosmos (CosmosHub), you could do that here
 }
 
@@ -17,30 +47,4 @@ export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
     timestamp: tx.block.block.header.time,
   });
   await transactionRecord.save();
-}
-
-export async function handleMessage(msg: CosmosMessage): Promise<void> {
-  const messageRecord = Fantoken.create({
-    id: `${msg.tx.hash}-${msg.idx}`,
-    blockHeight: BigInt(msg.block.block.header.height),
-    txHash: msg.tx.hash,
-    symbol: msg.msg.decodedMsg.symbol,
-  });
-  await messageRecord.save();
-}
-
-export async function handleEvent(event: CosmosEvent): Promise<void> {
-  const eventRecord = new FantokenIssueEvent(`${event.tx.hash}-${event.msg.idx}-${event.idx}`,);
-  eventRecord.blockHeight = BigInt(event.block.block.header.height);
-  eventRecord.txHash = event.tx.hash;
-  for(const attr of event.event.attributes) {
-    switch(attr.key) {
-      case "denom":
-        eventRecord.denom = attr.value.slice(1, -1);
-        break;
-      default:
-        break;
-    }
-  }
-  await eventRecord.save();
-}
+}*/
