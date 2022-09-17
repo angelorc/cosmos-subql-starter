071c52e511f8fa1f8042bc20d2338dd93455dd58
angelorc <angelo@rcstart.it>
2022-09-17 16:48:48 +0200

    chunk swap events

--- Osmosis/src/mappings/mappingHandlers.ts
+++ Osmosis/src/mappings/mappingHandlers.ts
@@ -2,32 +2,38 @@ import { TokenSwapEvent, Coin } from "../types";
 import { CosmosEvent } from "@subql/types-cosmos";
 
 export async function handleEventTokenSwapped(event: CosmosEvent): Promise<void> {
-  const evt = new TokenSwapEvent(`${event.tx.hash}-${event.msg.idx}-${event.idx}`);    
+  const chunkSize = 5;
+
+  for (let i = 0; i < event.event.attributes.length; i += chunkSize) {
+      const chunk = event.event.attributes.slice(i, i + chunkSize);
+      
+      const evt = new TokenSwapEvent(`${event.tx.hash}-${event.msg.idx}-${event.idx}-${i}`);    
   
-  evt.blockHeight = BigInt(event.block.block.header.height);
-  evt.blockTime = new Date(event.block.block.header.time);
-  evt.txHash= event.tx.hash;
+      evt.blockHeight = BigInt(event.block.block.header.height);
+      evt.blockTime = new Date(event.block.block.header.time);
+      evt.txHash = event.tx.hash;
 
-  for(const attr of event.event.attributes) {
-    switch(attr.key) {
-      case "sender":
-        evt.sender = attr.value;
-        break;
-      case "pool_id":
-        evt.poolId = BigInt(attr.value);
-        break;
-      case "tokens_in":
-        evt.tokensIn = parseCoins(attr.value)[0];
-        break;
-      case "tokens_out":
-        evt.tokensOut = parseCoins(attr.value)[0];
-        break;
-      default:
-        break;
-    }
+      for(const attr of chunk) {
+        switch(attr.key) {
+          case "sender":
+            evt.sender = attr.value;
+            break;
+          case "pool_id":
+            evt.poolId = BigInt(attr.value);
+            break;
+          case "tokens_in":
+            evt.tokensIn = parseCoins(attr.value)[0];
+            break;
+          case "tokens_out":
+            evt.tokensOut = parseCoins(attr.value)[0];
+            break;
+          default:
+            break;
+        }
+      }
+    
+      await evt.save();
   }
-
-  await evt.save();
 }
 
 export function parseCoins(input: string): Coin[] {
