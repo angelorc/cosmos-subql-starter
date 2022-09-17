be1c4b4574515ea201c26abaa65d08704d3a3587
Angelo <angelo@rcstart.it>
2022-09-17 23:25:51 +0200

    introducing lockup

--- Osmosis/src/mappings/mappingHandlers.ts
+++ Osmosis/src/mappings/mappingHandlers.ts
@@ -1,20 +1,56 @@
-import { TokenSwapEvent, Coin, LiquidityEvent } from "../types";
+import { TokenSwapEvent, Coin, LiquidityEvent, LockTokensEvent, AddTokensToLockEvent } from "../types";
 import { CosmosEvent } from "@subql/types-cosmos";
 
 export async function handleTokenSwappedEvent(event: CosmosEvent): Promise<void> {
   const chunkSize = 5;
 
   for (let i = 0; i < event.event.attributes.length; i += chunkSize) {
-      const chunk = event.event.attributes.slice(i, i + chunkSize);
-      
-      const evt = new TokenSwapEvent(`${event.tx.hash}-${event.msg.idx}-${event.idx}-${i}`);    
-  
+    const chunk = event.event.attributes.slice(i, i + chunkSize);
+
+    const evt = new TokenSwapEvent(`${event.tx.hash}-${event.msg.idx}-${event.idx}-${i}`);
+
+    evt.blockHeight = BigInt(event.block.block.header.height);
+    evt.blockTime = new Date(event.block.block.header.time);
+    evt.txHash = event.tx.hash;
+
+    for (const attr of chunk) {
+      switch (attr.key) {
+        case "sender":
+          evt.sender = attr.value;
+          break;
+        case "pool_id":
+          evt.poolId = BigInt(attr.value);
+          break;
+        case "tokens_in":
+          evt.tokensIn = parseCoins(attr.value)[0];
+          break;
+        case "tokens_out":
+          evt.tokensOut = parseCoins(attr.value)[0];
+          break;
+        default:
+          break;
+      }
+    }
+
+    await evt.save();
+  }
+}
+
+export async function handleLiquidityEvent(event: CosmosEvent): Promise<void> {
+  const chunkSize = 4;
+
+  for (let i = 0; i < event.event.attributes.length; i += chunkSize) {
+    const chunk = event.event.attributes.slice(i, i + chunkSize);
+
+    if (chunk.length == chunkSize) {
+      const evt = new LiquidityEvent(`${event.tx.hash}-${event.msg.idx}-${event.idx}-${i}`);
+
       evt.blockHeight = BigInt(event.block.block.header.height);
       evt.blockTime = new Date(event.block.block.header.time);
       evt.txHash = event.tx.hash;
 
-      for(const attr of chunk) {
-        switch(attr.key) {
+      for (const attr of chunk) {
+        switch (attr.key) {
           case "sender":
             evt.sender = attr.value;
             break;
@@ -24,50 +60,107 @@ export async function handleTokenSwappedEvent(event: CosmosEvent): Promise<void>
           case "tokens_in":
             evt.tokensIn = parseCoins(attr.value)[0];
             break;
-          case "tokens_out":
-            evt.tokensOut = parseCoins(attr.value)[0];
+          default:
+            break;
+        }
+      }
+
+      await evt.save();
+    }
+  }
+}
+
+export async function handleLockTokensEvent(event: CosmosEvent): Promise<void> {
+  const chunkSize = 6;
+
+  for (let i = 0; i < event.event.attributes.length; i += chunkSize) {
+    const chunk = event.event.attributes.slice(i, i + chunkSize);
+
+    const evt = new LockTokensEvent(`${event.tx.hash}-${event.msg.idx}-${event.idx}-${i}`);
+
+    evt.blockHeight = BigInt(event.block.block.header.height);
+    evt.blockTime = new Date(event.block.block.header.time);
+    evt.txHash = event.tx.hash;
+
+    logger.info(`
+    
+    ${event.tx.hash}
+    ${chunk.length}
+    
+    `)
+
+    if (chunk.length == 5) {
+      for (const attr of chunk) {
+        switch (attr.key) {
+          case "period_lock_id":
+            evt.periodLockId = attr.value;
+            break;
+          case "owner":
+            evt.owner = attr.value;
+            break;
+          case "amount":
+            evt.amount = attr.value;
+            break;
+          case "duration":
+            evt.duration = attr.value;
+            break;
+          case "unlock_time":
+            evt.unlockTime = attr.value;
             break;
           default:
             break;
         }
       }
-    
+
       await evt.save();
+    }
+    // ExtendLockup
+    else if (chunk.length == 3) {
+      for (const attr of chunk) {
+        switch (attr.key) {
+          case "period_lock_id":
+            evt.periodLockId = attr.value;
+            break;
+          case "owner":
+            evt.owner = attr.value;
+            break;
+          case "duration":
+            evt.duration = attr.value;
+            break;
+          default:
+            break;
+        }
+      }
+
+      await evt.save();
+    }
   }
 }
 
-export async function handleLiquidityEvent(event: CosmosEvent): Promise<void> {
-  const chunkSize = 4;
+export async function handleAddTokensToLockEvent(event: CosmosEvent): Promise<void> {
+  const evt = new AddTokensToLockEvent(`${event.tx.hash}-${event.msg.idx}-${event.idx}`);
 
-  for (let i = 0; i < event.event.attributes.length; i += chunkSize) {
-      const chunk = event.event.attributes.slice(i, i + chunkSize);
+  evt.blockHeight = BigInt(event.block.block.header.height);
+  evt.blockTime = new Date(event.block.block.header.time);
+  evt.txHash = event.tx.hash;
 
-      if (chunk.length == chunkSize) {
-        const evt = new LiquidityEvent(`${event.tx.hash}-${event.msg.idx}-${event.idx}-${i}`);    
-  
-        evt.blockHeight = BigInt(event.block.block.header.height);
-        evt.blockTime = new Date(event.block.block.header.time);
-        evt.txHash = event.tx.hash;
-
-        for(const attr of chunk) {
-          switch(attr.key) {
-            case "sender":
-              evt.sender = attr.value;
-              break;
-            case "pool_id":
-              evt.poolId = BigInt(attr.value);
-              break;
-            case "tokens_in":
-              evt.tokensIn = parseCoins(attr.value)[0];
-              break;
-            default:
-              break;
-          }
-        }
-      
-        await evt.save();
-      }
+  for (const attr of event.event.attributes) {
+    switch (attr.key) {
+      case "period_lock_id":
+        evt.periodLockId = attr.value;
+        break;
+      case "owner":
+        evt.owner = attr.value;
+        break;
+      case "amount":
+        evt.amount = attr.value;
+        break;
+      default:
+        break;
+    }
   }
+
+  await evt.save();
 }
 
 export function parseCoins(input: string): Coin[] {
@@ -85,3 +178,4 @@ export function parseCoins(input: string): Coin[] {
       };
     });
 }
+
